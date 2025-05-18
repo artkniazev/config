914d75f9c386ed9ed227794d62728bca272ada10
artem.e.knyazev <artem.e.knyazev@yandex.ru>
2025-05-18 22:06:00 +0300

    emacs: housekeeping, moved options from custom to own

--- emacs/init.el
+++ emacs/init.el
@@ -1,23 +1,32 @@
 ;;; -*- lexical-binding: t -*-
+;; https://www.melpa.org/#/getting-started
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(custom-set-variables
- '(blink-cursor-mode nil) ; don't blink cursor
- '(column-number-mode t) ; show column numbers
- '(custom-enabled-themes '(wheatgrass))
- '(global-display-line-numbers-mode t) ; show static line numbers
- '(scroll-bar-mode nil) ; hide scrollbar
- '(tool-bar-mode nil) ; hide tool bar
- '(menu-bar-mode nil) ; hide menu bar
- '(tooltip-mode nil)) ; hide tooltip
+;; https://wikemacs.org/wiki/Smex
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay when Smex is auto-initialized on its first run.
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
-(custom-set-faces
- '(default ((t (
-		:family "JetBrainsMono Nerd Font Mono"
-		:foundry "nil"
-	    	:slant normal
-       		:weight medium
-		:height 150
-	       	:width normal)))))
+;; https://www.masteringemacs.org/article/introduction-to-ido-mode
+;; Interactive Do It mode; pressing tab shows autocompletion
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(set-frame-font "JetBrainsMono Nerd Font Mono 15" nil t)
+(load-theme 'wheatgrass t)
+(blink-cursor-mode 0)
+(tool-bar-mode 0)
+(tooltip-mode 1)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+(show-paren-mode 1)
+(column-number-mode t)
+(global-display-line-numbers-mode t)
 
 ;; find-file-at-point bindings to use C-x C-f to go to files under cursor
 ;; see https://stackoverflow.c:om/a/259376
@@ -31,10 +40,12 @@
 (auto-save-visited-mode +1)
 (setq auto-save-visited-interval 1)
 
+;; https://emacsredux.com/blog/2020/12/04/maximize-the-emacs-frame-on-startup/
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
 ;; https://arne.me/blog/emacs-from-scratch-part-one-foundations
 ;; https://www.reddit.com/r/emacs/comments/cbkmde/comment/eth2wl3
 ;; hide startup message, open scratch at startup
-
 (use-package emacs
   :init
   (setq-default inhibit-startup-screen t)
@@ -42,13 +53,9 @@
   (setq inhibit-startup-message t)
   (setq initial-scratch-message nil)
   (setq initial-major-mode 'fundamental-mode)
+  (defalias 'yes-or-no-p 'y-or-n-p)
   (defun display-startup-echo-area-message ()
     (message "")))
-
-;; use y or n in confirmation messages
-(use-package emacs
-  :init
-  (defalias 'yes-or-no-p 'y-or-n-p))
 
 ;; use utf-8
 (use-package emacs
