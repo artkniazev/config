b89b146ae1865f753f9613e432c06be4340c8286
artem.e.knyazev <artem.e.knyazev@yandex.ru>
2025-05-19 22:17:27 +0300

    emacs: packages, housekeeping

--- emacs/init.el
+++ emacs/init.el
@@ -1,14 +1,26 @@
 ;;; -*- lexical-binding: t -*-
+
+;; https://www.melpa.org/#/getting-started
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; https://emacs.stackexchange.com/a/28933
+(dolist (package '(use-package smex magit go-mode))
+ (unless (package-installed-p package)
+   (package-install package)))
+
+;; Custom options
 (setopt user-full-name "Artem E. Knyazev"
 	user-mail-address "artem.e.knyazev@gmail.com")
 
 (setq custom-file "~/.config/emacs/custom.el") ; custom options go to a separate file
 (load-file custom-file)
-(set-frame-font "JetBrainsMono Nerd Font Mono 15" nil t)
+(set-frame-font "JetBrainsMono Nerd Font Mono 16" nil t)
 (load-theme 'wheatgrass t)
 (blink-cursor-mode 0)
-(tool-bar-mode 0)
 (tooltip-mode 1)
+(tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (column-number-mode 1)
@@ -17,16 +29,63 @@
 (global-display-line-numbers-mode t)
 (setq-default display-line-numbers-width 3)
 
-(when (string= system-type "darwin")       
-  (setq dired-use-ls-dired nil))
+;; Mute a follow symlinks question
+;; https://stackoverflow.com/a/30900018
+(setq vc-follow-symlinks t)
+
+;; Preserve hard links
+;; https://emacs.stackexchange.com/a/4240
+(setq backup-by-copying-when-linked t)
+
+;; Map C-x C-b to ibuffer instead
+(global-set-key [remap list-buffers] 'ibuffer)
+
+;; Restore Home&End keys behavior on MacOS
+;; https://www.reddit.com/r/emacs/comments/j6vwa3/comment/g8111mv
+(global-set-key (kbd "<home>") 'move-beginning-of-line)
+(global-set-key (kbd "<end>") 'move-end-of-line)
+
+;; Wrap search -- when fail type again C-s to continue from above
+;; https://stackoverflow.com/a/287067
+(defadvice isearch-search (after isearch-no-fail activate)
+  (unless isearch-success
+    (ad-disable-advice 'isearch-search 'after 'isearch-no-fail)
+    (ad-activate 'isearch-search)
+    (isearch-repeat (if isearch-forward 'forward))
+    (ad-enable-advice 'isearch-search 'after 'isearch-no-fail)
+    (ad-activate 'isearch-search)))
+;; Prevents issue where you have to press backspace twice when
+;; trying to remove the first character that fails a search
+(define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+
+;; Don't beep. Just blink the modeline on errors.
+;; https://web.archive.org/web/20241218055416/http://whattheemacsd.com/appearance.el-02.html
+(setq ring-bell-function (lambda ()
+                           (invert-face 'mode-line)
+                           (run-with-timer 0.05 nil 'invert-face 'mode-line)))
+
+;; C-RET inserts line below, doesn't split the line; C-S-RET inserts line above, same
+;; https://web.archive.org/web/20241218055416/http://whattheemacsd.com/editing-defuns.el-01.html
+(global-set-key
+ (kbd "<C-return>")
+ (lambda ()
+   (interactive)
+   (end-of-line)
+   (newline)
+   (indent-for-tab-command)))
+
+(global-set-key
+ (kbd "<C-S-return>")
+ (defun open-line-above ()
+   (interactive)
+   (beginning-of-line)
+   (newline)
+   (forward-line -1)
+   (indent-for-tab-command)))
 
 ;; https://emacsredux.com/blog/2022/06/03/enable-mouse-support-in-terminal-emacs/
 (xterm-mouse-mode 1)
 
-;; https://www.melpa.org/#/getting-started
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
 
 ;; https://wikemacs.org/wiki/Smex
 (require 'smex) ; Not needed if you use package.el
@@ -34,17 +93,18 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
-;; https://www.masteringemacs.org/article/introduction-to-ido-mode
-;; Interactive Do It mode; pressing tab shows autocompletion
-;(setq ido-enable-flex-matching t)
-;(setq ido-everywhere t)
-;(ido-mode 1)
+;; FIDO
+(fido-mode 1)
+;(fido-vertical-mode)
 
-;; find-file-at-point bindings to use C-x C-f to go to files under cursor
-;; see https://stackoverflow.c:om/a/259376
-;(ffap-bindings)
+;; windows
+;(global-set-key (kbd "M-o") 'other-window)
+;; move between windows w Shift+arrows
+;(windmove-default-keybindings)
 
 ;; global auto revert to reload files when they are changed on disk
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
 (global-auto-revert-mode 1)
 
 ;; https://www.reddit.com/r/emacs/comments/1aufydk/comment/kr4qu0v
@@ -53,7 +113,7 @@
 (setq auto-save-visited-interval 1)
 
 ;; https://emacsredux.com/blog/2020/12/04/maximize-the-emacs-frame-on-startup/
-(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+;(add-hook 'window-setup-hook 'toggle-frame-maximized t)
 
 ;; https://arne.me/blog/emacs-from-scratch-part-one-foundations
 ;; https://www.reddit.com/r/emacs/comments/cbkmde/comment/eth2wl3
@@ -87,14 +147,14 @@
 ;; https://www.reddit.com/r/emacs/comments/kbqdj7/comment/gfkodwo
 (require 'cl-lib)
 (cl-loop
-  for from across "1234567890-=йцукенгшщзхъфывапролджэёячсмитьбю/!\"№%:,.;()_+ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ?"
-  for to   across "1234567890-=qwertyuiop[]asdfghjkl;'\\zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"|ZXCVBNM<>?"
-  do
-  (define-key function-key-map
-    (kbd (string from)) (kbd (string to)))
-  (define-key function-key-map
-    (kbd (concat "C-" (string from))) (kbd (concat "C-" (string to))))
-  (define-key function-key-map
-    (kbd (concat "M-" (string from))) (kbd (concat "M-" (string to))))
-  (define-key function-key-map
-    (kbd (concat "C-M-" (string from))) (kbd (concat "C-M-" (string to)))))
+ for from across "1234567890-=йцукенгшщзхъфывапролджэёячсмитьбю/!\"№%:,.;()_+ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ?"
+ for to   across "1234567890-=qwertyuiop[]asdfghjkl;'\\zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"|ZXCVBNM<>?"
+ do
+ (define-key function-key-map
+	     (kbd (string from)) (kbd (string to)))
+ (define-key function-key-map
+	     (kbd (concat "C-" (string from))) (kbd (concat "C-" (string to))))
+ (define-key function-key-map
+	     (kbd (concat "M-" (string from))) (kbd (concat "M-" (string to))))
+ (define-key function-key-map
+	     (kbd (concat "C-M-" (string from))) (kbd (concat "C-M-" (string to)))))
